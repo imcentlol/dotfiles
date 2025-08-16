fa4b7bcc711fc5cb98a47f7ac1bceb15bf0aef99
imcentlol <imcentlol@proton.me>
2025-08-16 10:33:28 +0700

    Updated install script and waybar icon

--- install-packages.sh
+++ install-packages.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # must run as root
-xargs yay -S --needed --noconfirm < ./exported-packages
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zs
-h-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugin
-s/zsh-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUS
-TOM/plugins/zsh-autocomplete
+yay -S --needed --noconfirm -< ./exported-packages
+cd ~
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 git clone https://github.com/Andeskjerf/waybar-module-pomodoro.git
 cd waybar-module-pomodoro
 cargo build --release
@@ -19,3 +17,4 @@ cargo build --release
 sudo chmod +x target/release/niri
 sudo mv target/release/niri /usr/bin/
 cd ~
+sudo npm install --global purer-prompt
