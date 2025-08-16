7b449d22c7b3517e867155368ab581c1741a6bd3
imcentlol <imcentlol@proton.me>
2025-08-16 23:48:04 +0700

    Switched to gruvbox retro theme & updated change.sh script

--- config/waybar/scripts/active.sh
+++ config/waybar/scripts/active.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+status=$(ps aux | grep -i dnd.sh | grep /bin/bash)
+if [ status != "" ]; then
+    killall dnd.sh
+    makoctl dismiss -h
+fi
 
-makoctl dismiss
 makoctl mode -s default && notify-send "Mode: Active" --icon="$HOME/.config/waybar/icons/archlinux-512.png" -t 1600
 sleep 1.5
 makoctl dismiss -h
--- config/waybar/scripts/dnd.sh
+++ config/waybar/scripts/dnd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-makoctl dismiss 
+status=$(ps aux | grep -i active.sh | grep /bin/bash)
+if [ status != "" ]; then
+    killall active.sh
+    makoctl dismiss -h
+fi
+
 makoctl mode -s dnd
 notify-send "Mode: DND" --icon="$HOME/.config/waybar/icons/archlinux-512.png" -t 1600 --urgency=critical
 sleep 1.5
